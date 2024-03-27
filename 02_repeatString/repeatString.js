4827c63dbea57d58885f932a09d43b3aeef11d5a
Greison <barcelogreison@gmail.com>
2024-03-27 23:29:39 +0800

    Finish first two js exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let newWord = '';
+    if(times < 0){
+        return 'ERROR';
+    }
+    for (let i = 1; i <= times; i++){
+        newWord += word;
+    } return newWord;
 };
 
 // Do not edit below this line
