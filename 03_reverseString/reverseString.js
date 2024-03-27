c99eb46bfd264ab4051622ec9281bc8dd10cf026
Greison <barcelogreison@gmail.com>
2024-03-27 23:40:41 +0800

    Finish third exercise: reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversedWord = '';
+    wordArray = word.split('');
+    for(let i = wordArray.length - 1; i >= 0; i--){
+        reversedWord += wordArray[i];
+    } return reversedWord;
 };
 
 // Do not edit below this line
