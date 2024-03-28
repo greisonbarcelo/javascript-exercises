0b1ec308169579a8ac0e27783d3ae1273e217be8
Greison <barcelogreison@gmail.com>
2024-03-28 21:35:07 +0800

    Complete 8th exercise calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let summation = 0;
+	arr.forEach(index => {
+    summation += index;
+  }); return summation;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mults = 1;
+	arr.forEach(index => {
+    mults *= index;
+  }); return mults;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  let numPowed = 1;
+	for(let i = 1; i <= pow; i++){
+    numPowed *= num;
+  } return numPowed;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let numFac = 1;
+  for(let i = num; i > 0; i--){
+    numFac *= i;
+  } 
+  if(numFac === 0){
+    numFac = 1;
+  }
+  return numFac;
 };
 
 // Do not edit below this line
