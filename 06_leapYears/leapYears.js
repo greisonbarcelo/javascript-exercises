61da982a8efece49c952e06c7c7ca84fdbec8552
Greison <barcelogreison@gmail.com>
2024-03-28 17:36:04 +0800

    Finish 6th exercise leapyears, using my own learn- ings from theOdinProject, but will update for the shorter and more concise solution due to easy to read and easy to understand nature!

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let exception = [100, 400];
+    let leap = 4;
 
+    if(year % exception[0] === 0 && year % exception[1] !== 0){
+        return false;
+    } else if(year % leap === 0 || year % exception[0] === 0 && year % exception[1] === 0){
+        return true;
+    } 
+    else return false;
 };
 
+    
+//leapYears(2000);
 // Do not edit below this line
 module.exports = leapYears;
