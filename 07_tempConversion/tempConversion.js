f8b899c4ece9e46a0f5c58267367f864f5dd4168
Greison <barcelogreison@gmail.com>
2024-03-28 20:53:20 +0800

    Finish 7th exercise convert temps using 2 function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let formulaOne = (fahrenheit - 32) * 5/9;
+
+  if(formulaOne === 0){
+    return formulaOne;
+  }
+  return parseFloat(formulaOne.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let formulaTwo = (9/5 * celsius) + 32;
+  if(Number.isInteger(formulaTwo)){
+    return formulaTwo;
+  }
+  return parseFloat(formulaTwo.toFixed(1));
 };
 
 // Do not edit below this line
