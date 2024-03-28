f0d29cfe67e69b0ec8608a21b9bc8217399528aa
Greison <barcelogreison@gmail.com>
2024-03-28 17:39:17 +0800

    Finish 6th exercise new function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,15 +1,18 @@
-const leapYears = function(year) {
-    let exception = [100, 400];
-    let leap = 4;
+// const leapYears = function(year) {
+//     let exception = [100, 400];
+//     let leap = 4;
 
-    if(year % exception[0] === 0 && year % exception[1] !== 0){
-        return false;
-    } else if(year % leap === 0 || year % exception[0] === 0 && year % exception[1] === 0){
-        return true;
-    } 
-    else return false;
-};
-
+//     if(year % exception[0] === 0 && year % exception[1] !== 0){
+//         return false;
+//     } else if(year % leap === 0 || year % exception[0] === 0 && year % exception[1] === 0){
+//         return true;
+//     } 
+//     else return false;
+// };
+//new code!
+const leapYears = function(year){
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+}
     
 //leapYears(2000);
 // Do not edit below this line
