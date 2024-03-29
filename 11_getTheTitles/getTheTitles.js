4fc082717292867c797472fe9ea29a694f9c561d
Greison <barcelogreison@gmail.com>
2024-03-29 18:12:08 +0800

    Complete 11th exercise, this was using my current knowledge, but I will try to learn the given solution to better understand the array.map func. My solution is pretty straightforward by looping all the items in the object and taking all the titles and putting them in a new array for only titles and then returning it.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
 const getTheTitles = function() {
-
+    let newArr = [];
+    for(let i = 0; i < books.length; i++){
+        newArr[i] = books[i].title;
+        //console.log(books[i].title);
+    } return newArr;
+    //console.log(newArr);
 };
-
+//getTheTitles();
 // Do not edit below this line
 module.exports = getTheTitles;
