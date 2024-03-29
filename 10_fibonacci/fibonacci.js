8ec0aaf80aa6c23d4a46180284883d910dba45bf
Greison <barcelogreison@gmail.com>
2024-03-29 17:28:36 +0800

    Complete 10th exercise fibonacci, these took me some time to figure out and some googling, but I managed to get the solution by setting the default value of first two numbers in fibonacci sequence as 0 and 1 and adding it then the value of '1' is reassigned to the old value of 'fib' then I add the new sum of 'fib' to the old value of '1' to continue the process of the fibonacci sequence.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num >= 0){
+        let fib = 0;
+        let defaultOne = 0;
+        let defaultTwo = 1;
+        for(let i = 1; i <= num; i++){
+            fib = defaultOne + defaultTwo;
+            defaultTwo = defaultOne;
+            defaultOne = fib;
+            //console.log(fib);
+        }
+        //console.log(fib);
+        return fib;
+    } else return "OOPS";
 };
 
+//fibonacci(7);
 // Do not edit below this line
 module.exports = fibonacci;
