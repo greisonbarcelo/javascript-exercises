29fe3cc29c78d4cb3c734b533f605a98fa9833eb
Greison <barcelogreison@gmail.com>
2024-03-29 10:48:37 +0800

    Complete 9th exercise, my own solution seems fine but the solution given gave me a new perspective that you can concatenate multiple built-in functs in a single variable assignment and pass them all there is a very efficient way of cleaning a string

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let rev = '';
+    let newWord = word.toLowerCase();
+    let punctuation = /[\.,?! ]/g;
+    let finalWord = newWord.replace(punctuation, "");
+    // console.log(finalWord);
+    for(let i = finalWord.length-1; i >= 0; i--){
+        rev += finalWord[i];
+    } 
+    // console.log(finalWord);
+    // console.log(rev);
+    // console.log(finalWord === rev);
+    return finalWord === rev;
 
+    
 };
-
+//palindromes('A car, a man, a maraca.');
 // Do not edit below this line
 module.exports = palindromes;
