3f3d294df011dd9d4e314c3bfd8a83ecbafa609b
Greison <barcelogreison@gmail.com>
2024-03-30 09:28:42 +0800

    Unfinished 12th exercise, currently stuck right

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+
+const findTheOldest = function(people) {
+    //let newArrayName = people.map((people) => people.name);
+    let newArrayAge = people.map((people) => people.yearOfDeath - people.yearOfBirth);
+    let oldestAge = Math.max(...newArrayAge);
+    
+
+    let oldestIndex = newArrayAge.indexOf(Math.max(...newArrayAge));
+    //console.log(people[oldestIndex].name);
+    return people[oldestIndex].name;
+    //console.log(people[oldestIndex].name);
+    //console.log(newArrayName);
+    // console.log(newArrayAge);
+    // console.log(oldestAge);
+    //let oldestAgeName = newArrayName[newArrayAge.indexOf(Math.max(...newArrayAge))];
+    //console.log(oldestAgeName)
+
 
 };
-
+//findTheOldest(people);
 // Do not edit below this line
 module.exports = findTheOldest;
